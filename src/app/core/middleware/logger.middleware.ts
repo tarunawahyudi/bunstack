ee8d4772d11ad172a018446ab24a7a15a14b66b1
Taruna Wahyudi <taruna.wahyudi@asetku.co.id>
2026-06-06 03:30:50 +0700

    fix: log middleware status code

--- src/app/core/middleware/logger.middleware.ts
+++ src/app/core/middleware/logger.middleware.ts
@@ -2,9 +2,9 @@ import { Elysia } from 'elysia'
 import { logger } from '@shared/util/logger.util'
 
 export const loggerMiddleware = (app: Elysia) =>
-  app.onAfterHandle(({ response, request }) => {
+  app.onAfterHandle(({ set, request }) => {
     const { method, url } = request
-    const status = (response as { code: number }).code
+    const status = set.status ?? 200
     const path = new URL(url).pathname
     const logMessage = `${method} ${path} ${status}`
     logger.info(logMessage)
